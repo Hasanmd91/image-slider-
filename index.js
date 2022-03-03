5ad2e80d80baa591eb3f1289498dbde9dd26ffe4
MD ABUL HASAN <hasanmd1991@hotmail.com>
2022-03-03 13:07:49 +0200

    deleted some photo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-var photos =["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg","images/7.jpg","images/8.jpg","images/9.jpg","images/10.jpg","images/11.jpg","images/12.jpg"]
+var photos =["images/1.jpg","images/2.jpg","images/3.jpg","images/4.jpg","images/5.jpg","images/6.jpg"]
 var imgTag = document.querySelector("img");
 
 count = 0;
